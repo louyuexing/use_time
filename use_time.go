0d9a9d8d56c2ad62f96ea070512d7b30d10a829e
kui <louyuexing@outlook.com>
2025-01-14 19:30:47 +0800

    feat: 新增cross

--- use_time.go
+++ use_time.go
@@ -4,14 +4,43 @@ import (
 	"time"
 )
 
+// UseTime is a struct for tracking and managing time-related information.
+// It contains the origin time, a function to get the current time, and the beginning time for calculating time differences.
 type UseTime struct {
-	t time.Time
+	origin time.Time        // The origin time, usually set when the instance is created.
+	t      func() time.Time // A function to get the current time, for obtaining the current time in a customizable way.
+	b      time.Time        // The beginning time, used as a reference for calculating time differences.
 }
 
-func New(begin time.Time) *UseTime {
-	return &UseTime{t: begin}
+// New creates and returns a pointer to a UseTime instance.
+// Parameter origin: The origin time, marking the starting point for time tracking.
+func New(origin time.Time) *UseTime {
+	// Initialize a UseTime instance, setting the t function to return the current time.
+	u := UseTime{
+		t: func() time.Time {
+			return time.Now()
+		},
+		origin: origin,
+	}
+	// Set the beginning time to the origin time.
+	u.b = u.origin
+	// Return the pointer to the UseTime instance.
+	return &u
 }
 
+// Step returns the time duration since the last call to Step or Cross.
+// It updates the internal beginning time to the current time.
 func (u *UseTime) Step() time.Duration {
-	return time.Since(u.t)
+	// Calculate the time difference since the last beginning time.
+	d := time.Since(u.b)
+	// Update the beginning time to the current time.
+	u.b = u.t()
+	// Return the calculated time difference.
+	return d
+}
+
+// Cross returns the time duration since the origin time.
+func (u *UseTime) Cross() time.Duration {
+	// Calculate the time difference since the origin time.
+	return time.Since(u.origin)
 }
--- use_time_test.go
+++ use_time_test.go
@@ -1,12 +1,14 @@
-# use time
-## use
-```
+package use_time
+
+import (
+	"testing"
+	"time"
+)
+
 func TestUseTime(t *testing.T) {
 	ut := New(time.Now())
 	time.Sleep(2 * time.Second)
 	t.Log(ut.Step().String())
 	time.Sleep(2 * time.Second)
 	t.Log(ut.Cross().String())
-	
 }
-```
